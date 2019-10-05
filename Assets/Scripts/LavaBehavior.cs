ded9b597b4f160cb830cac3cbfd7debef89351bc
Ville Nuutinen <ville.nuutinen@wunder.dog>
2019-10-05 18:34:04 +0300

    Added texture to world and lava

--- Assets/Scripts/LavaBehavior.cs
+++ Assets/Scripts/LavaBehavior.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LavaBehavior : MonoBehaviour
 {
     private Material _material;
+    public Vector2 mainTexOffset;
+    public Vector2 secondaryTexOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +20,10 @@ public class LavaBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float offset = Time.time;
+        float timeOffset = Time.time;
         Debug.Log(String.Join(",", _material.GetTexturePropertyNames().AsEnumerable()));
-        _material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
-        _material.SetTextureOffset("_DetailAlbedoMap", new Vector2(-offset/2, offset/4));
+        _material.SetTextureOffset("_MainTex", mainTexOffset * timeOffset);
+        _material.SetTextureOffset("_DetailAlbedoMap", secondaryTexOffset * timeOffset);
 
 
     }
