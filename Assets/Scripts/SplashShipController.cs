4e53789c7537b0b87f3cb6260a701ff6b49dcbe1
Jere Kankaanranta <jere@kankje.com>
2019-10-05 20:01:51 +0300

    Remove accidentally added file

--- Assets/Scripts/SplashShipController.cs
+++ Assets/Scripts/SplashShipController.cs
@@ -7,16 +7,9 @@ public class SplashShipController : MonoBehaviour
 {
     private Rigidbody _body;
 
-    // Start is called before the first frame update
     void Start()
     {
         _body = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void FixedUpdate()
