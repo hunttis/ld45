eaccf622ea0613f9ddc9a4bd652d8217f1bb524d
Niclas Lindgren <niclas.lindgren@wunderdog.fi>
2019-10-05 12:31:26 +0300

    Add player movement and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rb;
+    public float speed = 10.0f;
+    public float jumpHeight = 2f;
+    public Vector3 movement;
+    public LayerMask ground;
+
+    private bool _isJumping;
+    private float _groundDistance;
+    private bool _isGrounded;
+    private Transform _groundChecker;
+
+    // Use this for initialization
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        _groundDistance = (float) (GetComponent<Renderer>().bounds.size.y * 0.55);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        _isGrounded = Physics.CheckSphere(rb.position, _groundDistance, ground, QueryTriggerInteraction.Ignore);
+        _isJumping = Input.GetButtonDown("Jump") && _isGrounded;
+    }
+
+    private void FixedUpdate()
+    {
+        Move(movement);
+        if (_isJumping) Jump();
+    }
+
+    private void Move(Vector3 direction)
+    {
+        rb.AddForce(direction * speed);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
     }
 }
