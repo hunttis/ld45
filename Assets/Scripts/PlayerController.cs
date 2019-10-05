64df2a095d767087f127c3c074dfa5f36bc7c17f
Teemu <teemu@rautasydan.org>
2019-10-05 15:47:27 +0300

    Player movement improvements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,42 +5,87 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    public static bool MouseLocked { get { return Cursor.lockState != CursorLockMode.None; }}
+
     public float speed = 10.0f;
     public float jumpHeight = 2f;
+    public Vector2 mouseSensitivity = new Vector2(2.0f, 0.4f);
     public Vector3 movement;
     public LayerMask ground;
+    public Transform CameraTarget;
+    public float cameraHeightMin = 0;
+    public float cameraHeightMax = 3.0f;
+
 
     private Rigidbody _rb;
     private bool _isJumping;
     private float _groundDistance;
     private bool _isGrounded;
     private Transform _groundChecker;
+    
+    private PlayerModel _playerModel;
 
-    // Use this for initialization
-    private void Start()
+    public static void LockCursor()
     {
-        _rb = GetComponent<Rigidbody>();
-        _groundDistance = (float) (GetComponent<Collider>().bounds.size.y * 0.55);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public static void ReleaseCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _groundDistance = GetComponent<Collider>().bounds.size.y * 0.55f;
+        _playerModel = FindObjectOfType<PlayerModel>();
+
+        LockCursor();
+    }
+    
     private void Update()
     {
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
         _isGrounded = Physics.CheckSphere(_rb.position, _groundDistance, ground, QueryTriggerInteraction.Ignore);
         _isJumping = Input.GetButtonDown("Jump") && _isGrounded;
+
+        if (MouseLocked)
+        {
+            _playerModel.transform.Rotate(0, Input.GetAxis("Mouse X")*mouseSensitivity.x, 0);
+            
+            if (CameraTarget != null)
+            {
+                Vector3 cameraTargetPos = CameraTarget.localPosition;
+                cameraTargetPos.y = Mathf.Clamp(cameraTargetPos.y + Input.GetAxis("Mouse Y")*mouseSensitivity.y, cameraHeightMin, cameraHeightMax);
+                CameraTarget.localPosition = cameraTargetPos;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.LeftControl)) ToggleMouseLock();
     }
+
+    private void ToggleMouseLock()
+    {
+        if (!MouseLocked)
+        {
+            LockCursor();
+        }
+        else
+        {
+            ReleaseCursor();
+        }
+}
 
     private void FixedUpdate()
     {
         Move(movement);
         if (_isJumping) Jump();
+        
     }
 
     private void Move(Vector3 direction)
     {
-        _rb.AddForce(direction * speed);
+        _rb.AddForce(_playerModel.transform.TransformDirection(direction) * speed);
     }
 
     private void Jump()
