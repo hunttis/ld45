4428a021dded82141d253ff823e5d74e6a33dfc8
Niclas Lindgren <niclas.lindgren@wunderdog.fi>
2019-10-05 16:54:28 +0300

    Change ResourceMagnet to use collider as trigger

--- Assets/Scripts/ResourceMagnet.cs
+++ Assets/Scripts/ResourceMagnet.cs
@@ -1,39 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class ResourceMagnet : MonoBehaviour
 {
-    public float maxDistance = 10.0f;
     public float magnetStrength = 10.0f;
 
-    private Rigidbody[] _resources;
-
-    private void Awake()
+    private void OnTriggerStay(Collider other)
     {
-        InvokeRepeating(nameof(UpdateResourceList), 0, 1);
-    }
+        if (other.GetComponent<Resource>() == null) return;
 
-    private void UpdateResourceList()
-    {
-        _resources = GameObject.FindGameObjectsWithTag("Resource").AsEnumerable()
-            .Select(obj => obj.GetComponent<Rigidbody>()).ToArray();
-    }
-
-    private void FixedUpdate()
-    {
+        var resourceBody = other.GetComponent<Rigidbody>();
         var magnetPosition = transform.position;
-        foreach (var resource in _resources)
-        {
-            var position = resource.position;
-            var distance = Vector3.Distance(magnetPosition, position);
-            if (distance > maxDistance) continue;
+        var position = resourceBody.position;
+        var distance = Vector3.Distance(magnetPosition, position);
+        var forceDirection = (magnetPosition - position).normalized;
 
-            var forceDirection = (magnetPosition - position).normalized;
-            resource.AddForce(forceDirection * (magnetStrength / distance));
-        }
+        resourceBody.AddForce(forceDirection * (magnetStrength / distance));
     }
 }
