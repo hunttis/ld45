2cd8a722ae84b254b9818f8ce3907cbd166007a4
Teemu <teemu@rautasydan.org>
2019-10-05 13:56:27 +0300

    Gravity change, Player ball rotation, World improvements

---
+++ Assets/Scripts/PlayerModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour
+{
+    PlayerController _ctrl;
+    Rigidbody _ctrlRb;
+    Transform _t;
+    private void Awake()
+    {
+        _ctrl = FindObjectOfType<PlayerController>();
+        if (_ctrl == null) Debug.LogError("PlayerController not found in scene!");
+        _ctrlRb = _ctrl.GetComponent<Rigidbody>();
+        _t = GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _t.position = _ctrl.transform.position;
+        Vector3 lookAtVector = _ctrlRb.velocity.normalized;
+        lookAtVector.y = 0;
+        _t.LookAt(_t.position + lookAtVector);
+    }
+}
---
+++ Assets/Scripts/PlayerWheelAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWheelAnimation : MonoBehaviour
+{
+    PlayerController _ctrl;
+    Transform _t;
+
+    private void Awake()
+    {
+        _t = GetComponent<Transform>();
+        _ctrl = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = _ctrl.transform.rotation;
+    }
+}
