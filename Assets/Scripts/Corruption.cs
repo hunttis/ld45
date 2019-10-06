4da6ec0db98de746a3f82f753c01935989831f16
Niclas Lindgren <niclas.lindgren@wunderdog.fi>
2019-10-06 14:16:53 +0300

    Add Corruption to Uranium

---
+++ Assets/Scripts/Corruption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Corruption : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        var corruptable = other.GetComponent<Corruptable>();
+        if (corruptable == null) return;
+        corruptable.Corrupt(Time.deltaTime);
+    }
+}
